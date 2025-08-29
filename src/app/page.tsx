9200591031e88e253848d7fc808a5feac858946c
iUnstable0 <me@iunstable0.com>
2025-08-29 23:17:40 +0700

    file picker + iamge previe!

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,56 @@
-import Image from "next/image";
+"use client";
+
+import { useState, useMemo } from "react";
+
+import axios from "axios";
+
 import styles from "./page.module.css";
+import Image from "next/image";
 
 export default function Home() {
+  const [file, setFile] = useState<File | null>(null);
+
+  const imageUrl = useMemo(() => {
+    if (!file) return null;
+    return URL.createObjectURL(file);
+  }, [file]);
+
   return (
     <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+      <div className={styles.header}>
+        <b>The Goodnight Goat Generator</b>
+      </div>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+      <input
+        type={"file"}
+        accept={"image/*"}
+        className={styles.fileInput}
+        multiple={false}
+        onInput={(e) => {
+          // alert("file");
+          const file = (e.target as HTMLInputElement).files?.[0];
+          if (!file) return;
+
+          setFile(file);
+        }}
+      />
+
+      <div className={styles.previewCtn}>
+        {!imageUrl && (
+          <div className={styles.noImage}>
+            selected image will be shown here
+          </div>
+        )}
+        {imageUrl && (
           <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+            src={imageUrl}
+            alt={"Image preview"}
+            width={100}
+            height={100}
+            className={styles.image}
           />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        )}
+      </div>
     </div>
   );
 }
